68f75160abf4f25969f9ec7453c1f8bcef6b4a0d
Tuan Anh Do <dotuananh15031999@gmail.com>
2022-09-29 21:12:46 +0700

    React Code Editor Demo

---
+++ src/components/CellListItem/cell-list-item.tsx
@@ -0,0 +1,34 @@
+import './cell-list-item.css';
+import { Cell } from '../../state';
+import CodeCell from '../CodeCell/code-cell';
+import ActionBar from '../ActionBar/action-bar';
+import TextEditor from '../TextEditor/text-editor';
+
+interface CellListItemProps {
+  cell: Cell;
+}
+
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
+  } else {
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
+  }
+
+  return <div className="cell-list-item">{child}</div>;
+};
+
+export default CellListItem;
